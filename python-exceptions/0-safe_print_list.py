f5997cbeb978adc277f6200eff81c79162267060
Paul Muwanguzi <p.muwanguzi@alustudent.com>
2024-11-09 06:23:00 -0800

    updated the PEP8 validation

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -11,6 +11,5 @@ def safe_print_list(my_list=[], x=0):
             count += 1
     except IndexError:
         pass
-    print()  # Move to the new line after printing elements
+    print()  
     return count
-
