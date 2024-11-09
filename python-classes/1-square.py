39869d8cba1e7f68fd3940d35a27dd8b62b5f298
Paul Muwanguzi <p.muwanguzi@alustudent.com>
2024-11-09 07:42:14 -0800

    updated

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -2,10 +2,11 @@
 
 """Defining a class Square."""
 
+
 class Square:
-    """This class is a class with a private instance size."""
-    
-    def __init__(self, size):
+"""This class is a class with a private instance size."""
+
+    def __init__(self, size:int):
         """Square with given size initialised."""
 
         self.__size = size
