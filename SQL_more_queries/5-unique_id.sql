ceddd9d6147186ba49a9bf33ad0efd9a70ce826b
Paul Muwanguzi <p.muwanguzi@alustudent.com>
2024-11-16 14:14:07 -0800

    updated

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,3 +1,5 @@
---a script that creates the table unique_id on your MySQL server.
-USE hbtn_test_db_5;
-CREATE TABLE IF NOT EXISTS unique_id (id INT UNIQUE DEFAULT 1, name VARCHAR(256));
+-- a script that creates the table unique_id on your MySQL server.
+CREATE TABLE IF NOT EXISTS unique_id (
+    id INT UNIQUE DEFAULT 1,
+    name VARCHAR(256)
+);
