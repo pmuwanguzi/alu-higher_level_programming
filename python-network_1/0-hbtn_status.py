2ad4781d75c7015b1c2e23006dc1bad88bafd423
Paul Muwanguzi <p.muwanguzi@alustudent.com>
2024-12-01 07:41:09 -0800

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
-#a Python script that fetches https://alu-intranet.hbtn.io/status
+"""A Python script that fetches https://alu-intranet.hbtn.io/status
+using the urllib package."""
+from urllib import request
 
-
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as response:
+    body = response.read()
+        body = response.read()
+        print("Body response:")
+        print("\t- type:", type(body))
+        print("\t- content:", body)
+        print("\t- utf8 content:", body.decode("utf-8"))
