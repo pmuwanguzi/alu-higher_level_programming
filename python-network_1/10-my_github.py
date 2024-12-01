7a0604b440d9cc58696f5041565265b35bdab951
Paul Muwanguzi <p.muwanguzi@alustudent.com>
2024-12-01 10:40:06 -0800

    errors

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -17,16 +17,6 @@ if __name__ == "__main__":
     Sends a GET request to the GitHub API using the provided credentials
     and displays the user ID.
     """
-    username = sys.argv[1]
-    token = sys.argv[2]
-
-    # Define the GitHub API URL
     url = "https://api.github.com/user"
-
-    # Send a GET request to the GitHub API with Basic Authentication
-    response = requests.get(url, auth=(username, token))
-
-    if response.status_code == 200:
-        # If the response is successful, print the user ID
-        user_data = response.json()
-        print(user_data.get("id"))
+    response = get(url, auth=(argv[1], argv[2]))
+    print(response.json().get("id"))
