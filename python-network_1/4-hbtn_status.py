127aad3f6260886260a2fc77974939f072506e57
Paul Muwanguzi <p.muwanguzi@alustudent.com>
2024-12-01 09:27:41 -0800

    4-hbtn_status.py

---
+++ python-network_1/4-hbtn_status.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+a Python script that fetches https://alu-intranet.hbtn.io/status
+"""
+
+
+import requests
+
+
+if __name__ == "__main__":
+    main()
+    """
+    Sends a GET request to 'https://alu-intranet.hbtn.io/status', get's back the
+    response then displays the type, content, and decoded content of the body
+    of the response.
+    """
+    url = 'https://alu-intranet.hbtn.io/status'
+
+    # Send a GET request to the URL
+    response = requests.get(url)
+
+    # Print the body of the response details
+    print("Body response:")
+    print("    - type: {}".format(type(response.text)))
+    print("    - content: {}".format(response.text))
+    print("    - utf8 content: {}".format(response.text.encode('utf-8').decode('utf-8')))
