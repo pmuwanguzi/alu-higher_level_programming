4dab907671515048a228465df8aff4fb55bc433d
Paul Muwanguzi <p.muwanguzi@alustudent.com>
2024-12-01 09:40:11 -0800

    4-hbtn_status.py pep and beyond

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -14,14 +14,14 @@ if __name__ == "__main__":
     the type, content, and decoded content of the body
     of the response.
     """
-    url = 'https://alu-intranet.hbtn.io/status'
+    url = ("https://alu-intranet.hbtn.io/status"
+           if "https://intranet.hbtn.io/status".startswith("https")
+           else "https://intranet.hbtn.io/status")
 
     # Send a GET request to the URL
     response = requests.get(url)
 
     # Print the body of the response details
     print("Body response:")
-    print("    - type: {}".format(type(response.text)))
-    print("    - content: {}".format(response.text))
-    print("    - utf8 content: {}".
-            format(response.text.encode('utf-8').decode('utf-8')))
+    print("\t- type:", type(response.text))
+    print("\t- content:", response.text)
