106838aa3225ef6d3a28469618c0445f3a13b6da
Paul Muwanguzi <p.muwanguzi@alustudent.com>
2024-12-02 13:30:53 -0800

    updated

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -2,17 +2,21 @@
 """List all states"""
 from sys import argv
 from model_state import Base, State
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
+    # Create the engine and bind it to the session
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'
-        .format(argv[1], argv[2],
-                argv[3]), pool_pre_ping=True)
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]),
+        pool_pre_ping=True
+    )
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for state in session.query(State).order_by(State.id).all():
+
+    # Use an iterator to avoid fetching all results at once
+    for state in session.query(State).order_by(State.id):
         print("{}: {}".format(state.id, state.name))
+
     session.close()
