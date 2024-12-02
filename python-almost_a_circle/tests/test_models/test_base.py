2277b47a74348e0b8b80167cd50a8c74941dfed0
Paul Muwanguzi <p.muwanguzi@alustudent.com>
2024-12-02 13:02:23 -0800

    bulk

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -4,9 +4,9 @@
 import os
 import unittest
 
-from models.base import Base
-from models.rectangle import Rectangle
-from models.square import Square
+from ..models.base import Base
+from ..models.rectangle import Rectangle
+from ..models.square import Square
 
 
 class TestBase(unittest.TestCase):
